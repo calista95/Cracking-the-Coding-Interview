079ac3930b31f7f34797be4a9debc6d232be07a3
calista95 <35706745+calista95@users.noreply.github.com>
2019-03-28 15:14:03 -0700

    Add files via upload

---
+++ ch1/1-1.py
@@ -0,0 +1,24 @@
+#Program 1 Chapter 1 
+#Name: Is Unique
+#Runtime: O(n^2)
+
+
+def isUniqueChars(mystr):
+    if len(mystr) > 128:
+        return False
+    for x in range(len(mystr)):
+        for y in range(len(mystr)):
+            if (x != y): #do not compare the same indices 
+                if mystr[x] == mystr[y]:
+                    return False
+    return True
+
+
+result = isUniqueChars("abc")
+print(str(result))
+
+result = isUniqueChars("aba")
+print(str(result))
+
+result = isUniqueChars("abcaaaaaa")
+print(str(result))
