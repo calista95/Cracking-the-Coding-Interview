d393c49e9db2a0ca2fa1f230b0efe03830af431c
Calista Wong <wongcal@oregonstate.edu>
2019-04-04 11:53:56 -0700

    add files:

---
+++ ch10/10-2.py
@@ -0,0 +1,14 @@
+#Group anagram
+#Write a method to sort an array of strings so that all the anagrams are next to each other 
+
+anagram(arr):
+    for x in arr:
+        print(x)
+
+
+
+def main():
+    arr=["oww","cat", "rat","car","tac", "tra", "cra", "atc", "wow"]
+
+if __name__ == "__main__":
+    main()
